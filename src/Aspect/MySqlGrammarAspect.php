48b200852add7a6755cc8c168c16dd0cd98cef49
stone.mac <shilei_zhang@163.com>
2021-03-25 16:16:06 +0800

    [Bug修复](develop): 修复ASPECT语法错误

--- src/Aspect/MySqlGrammarAspect.php
+++ src/Aspect/MySqlGrammarAspect.php
@@ -12,6 +12,7 @@ namespace MoChat\Framework\Aspect;
 
 use Hyperf\Di\Annotation\Aspect;
 use Hyperf\Di\Aop\AbstractAspect;
+use Hyperf\Di\Aop\ProceedingJoinPoint;
 
 /**
  * @Aspect
@@ -29,7 +30,7 @@ class MySqlGrammarAspect extends AbstractAspect
      * 兼容mysql8.
      * @return string ...
      */
-    public function process(): string
+    public function process(ProceedingJoinPoint $proceedingJoinPoint): string
     {
         return 'select `column_key` as `column_key`, `column_name` as `column_name`, `data_type` as `data_type`, `column_comment` as `column_comment`, `extra` as `extra`, `column_type` as `column_type` from information_schema.columns where `table_schema` = ? and `table_name` = ? order by ORDINAL_POSITION';
     }
